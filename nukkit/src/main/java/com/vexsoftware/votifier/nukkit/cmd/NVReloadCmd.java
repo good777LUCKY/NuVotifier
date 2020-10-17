dd5c53f002bae768a9d2de841bc8a37e5defe9d1
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-10-17 18:04:50 +0900

    Update NVReloadCmd.java

--- nukkit/src/main/java/com/vexsoftware/votifier/nukkit/cmd/NVReloadCmd.java
+++ nukkit/src/main/java/com/vexsoftware/votifier/nukkit/cmd/NVReloadCmd.java
@@ -6,6 +6,9 @@ import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.utils.TextFormat;
 
+/**
+ * @author good777LUCKY
+ */
 public class NVReloadCmd extends Command {
 
     private final NuVotifier plugin;
