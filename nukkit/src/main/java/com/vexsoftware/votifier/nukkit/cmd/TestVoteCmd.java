429c1c4048b759ab309c8975fde89773c08e4dc6
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-10-17 18:01:04 +0900

    Create TestVoteCmd.java

---
+++ nukkit/src/main/java/com/vexsoftware/votifier/nukkit/cmd/TestVoteCmd.java
@@ -0,0 +1,45 @@
+package com.vexsoftware.votifier.nukkit.cmd;
+
+import com.vexsoftware.votifier.model.Vote;
+import com.vexsoftware.votifier.net.VotifierSession;
+import com.vexsoftware.votifier.nukkit.NuVotifier;
+import com.vexsoftware.votifier.util.ArgsToVote;
+
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.utils.TextFormat;
+
+public class TestVoteCmd extends Command {
+
+    private final NuVotifier plugin;
+    
+    public TestVoteCmd(NuVotifier plugin) {
+        super("testvote", "Sends a test vote to the server", "/testvote [username] [serviceName=?] [username=?] [address=?] [localTimestamp=?] [timestamp=?]");
+        this.setPermission("nuvotifier.testvote");
+        this.commandParameters.clear();
+        
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String s, String[] args) {
+        if (!this.testPermission(sender)) {
+            sender.sendMessage(TextFormat.DARK_RED + "You do not have permission to do this!");
+            return true;
+        }
+        
+        Vote v;
+        try {
+            v = ArgsToVote.parse(args);
+        } catch (IllegalArgumentException e) {
+            sender.sendMessage(TextFormat.DARK_RED + "Error while parsing arguments to create test vote: " + e.getMessage());
+            sender.sendMessage(TextFormat.GRAY + "Usage hint: /testvote [username] [serviceName=?] [username=?] [address=?] [localTimestamp=?] [timestamp=?]");
+            return true;
+        }
+        
+        plugin.onVoteReceived(v, VotifierSession.ProtocolVersion.TEST, "localhost.test");
+        sender.sendMessage(TextFormat.GREEN + "Test vote executed: " + v.toString());
+        
+        return true;
+    }
+}
