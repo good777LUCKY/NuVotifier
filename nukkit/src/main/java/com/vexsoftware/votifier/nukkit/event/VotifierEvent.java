99f11828d092dfec6faa546dd1bff3a65e437b41
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-10-17 19:16:26 +0900

    Create VotifierEvent.java

---
+++ nukkit/src/main/java/com/vexsoftware/votifier/nukkit/event/VotifierEvent.java
@@ -0,0 +1,34 @@
+package com.vexsoftware.votifier.nukkit.event;
+
+import com.vexsoftware.votifier.model.Vote;
+
+import cn.nukkit.event.Event;
+import cn.nukkit.event.HandlerList;
+
+/**
+ * @author good777LUCKY
+ */
+public class VotifierEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+    
+    private Vote vote;
+    
+    public VotifierEvent(final Vote vote) {
+        this.vote = vote;
+    }
+    
+    
+    public Vote getVote() {
+        return vote;
+    }
+    
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
